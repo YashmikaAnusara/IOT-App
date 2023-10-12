def995bc207fa5acc7581e8402935e7a4e38d743
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-10-13 02:59:01 +0530

    bug

--- IOT/pages/navpages/parkingLight.js
+++ IOT/pages/navpages/parkingLight.js
@@ -1,7 +1,27 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import MQTT from 'sp-react-native-mqtt';
 
 export default function ParkingLight() {
+  const [test, settest] = useState(null);
+  const mqttClient = '';
+
+  MQTT.createClient({
+    uri: 'mqtt://192.168.1.3:1883',
+    clientId: 'your_client_id',
+  })
+    .then(function (client) {
+      client.on('connect', function () {
+        console.log('connected');
+        client.subscribe('Models/Detection/Sensor', 0);
+        settest(client);
+      });
+      client.connect();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
   return (
     <View>
       <Text>parkingLight</Text>
