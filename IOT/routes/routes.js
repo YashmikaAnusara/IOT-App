9cbcf9457d2c07912426c70639cf7833597ab45c
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-10-12 22:08:36 +0530

    iot

---
+++ IOT/routes/routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function Routes() {
+  return (
+    <div>routes</div>
+  )
+}
