fbe2c5ede757048e801079b312194e9940e34225
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-10-12 22:26:16 +0530

    iot

---
+++ IOT/pages/navpages/parkingLight.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function parkingLight() {
+  return (
+    <View>
+      <Text>parkingLight</Text>
+    </View>
+  )
+}
