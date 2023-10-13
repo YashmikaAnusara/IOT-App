d0b11713deeb88c8142f1bce6729d4e666daa226
NaveenHareendra <it20032524@my.sliit.lk>
2023-10-13 14:28:45 +0530

    MQTT Client Fix

--- IOT/pages/navpages/parkingLight.js
+++ IOT/pages/navpages/parkingLight.js
@@ -1,7 +1,31 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import MQTT from 'sp-react-native-mqtt';
 
 export default function ParkingLight() {
+  const [test, settest] = useState(null);
+  const mqttClient = '';
+
+  MQTT.createClient({
+    uri: 'mqtt://192.168.1.14:1883',
+    clientId: 'your_client_id',
+  })
+    .then(function (client) {
+ 
+      client.on('connect', function () {
+        console.log('connected');
+        client.subscribe('esp32/distance', 0);
+      });
+      client.on('message', function(msg) {
+        console.log('mqtt.event.message', msg.data);
+      });
+     
+      client.connect();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
   return (
     <View>
       <Text>parkingLight</Text>
