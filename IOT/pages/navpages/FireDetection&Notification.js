542ea77eee0b0271b03e920ee9c96f4effe85c78
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-10-15 04:30:42 +0530

    FireDetectionNotification

---
+++ IOT/pages/navpages/FireDetection&Notification.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function FireDetectionNotification() {
+  return (
+    <View>
+      <Text>Fire Detection & Notification</Text>
+    </View>
+  );
+}
