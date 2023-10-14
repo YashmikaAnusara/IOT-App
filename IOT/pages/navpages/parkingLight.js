4552eb7dd01e004f2ed70bc6f83f70295d0bb417
NaveenHareendra <it20032524@my.sliit.lk>
2023-10-14 15:17:52 +0530

    new some changes

--- IOT/pages/navpages/parkingLight.js
+++ IOT/pages/navpages/parkingLight.js
@@ -1,13 +1,13 @@
 import {View, Text} from 'react-native';
 import React, {useState} from 'react';
 import MQTT from 'sp-react-native-mqtt';
-
+import { Button } from 'react-native';
 export default function ParkingLight() {
   const [test, settest] = useState(null);
   const mqttClient = '';
 
   MQTT.createClient({
-    uri: 'mqtt://192.168.1.14:1883',
+    uri: 'mqtt://192.168.161.215:1883',
     clientId: 'your_client_id',
   })
     .then(function (client) {
@@ -28,6 +28,34 @@ export default function ParkingLight() {
 
   return (
     <View>
+      <Button
+          title="test"
+          onPress={() => {
+            //   console.log(mqttClient);
+            // client.publish('esp32/output', 'on', 0, true);
+
+            MQTT.createClient({
+              uri: 'mqtt://192.168.161.215:1883',
+              clientId: 'your_client_id',
+            })
+              .then(function (client) {
+           
+                client.on('connect', function () {
+                  console.log('connected');
+                  client.publish('esp32/output', 'on', 0, true);
+                });
+                client.on('message', function(msg) {
+                  console.log('mqtt.event.message', msg.data);
+                });
+               
+                client.connect();
+              })
+              .catch(function (err) {
+                console.log(err);
+              });
+          }}
+        />
+      
       <Text>parkingLight</Text>
     </View>
   );
