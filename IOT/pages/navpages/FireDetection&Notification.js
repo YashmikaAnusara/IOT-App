1ddc7d1d76ce4ee8e832096b708b05de73ea0152
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-10-15 12:30:32 +0530

    app

--- IOT/pages/navpages/FireDetection&Notification.js
+++ IOT/pages/navpages/FireDetection&Notification.js
@@ -1,10 +1,22 @@
-import {View, Text} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Header from '../../component/header';
 
 export default function FireDetectionNotification() {
   return (
-    <View>
-      <Text>Fire Detection & Notification</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.screenContainer}>
+          <Header />
+          <Text>Fire Detection & Notification</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 10,
+  },
+});
