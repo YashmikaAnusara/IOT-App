c5a03577308e612d80f0dc6b8a9213b694df61c7
Seshmi Senadheera <86178538+Seshmi@users.noreply.github.com>
2023-10-16 09:50:04 +0530

    Add uri to Fire Detection

--- IOT/pages/navpages/FireDetection&Notification.js
+++ IOT/pages/navpages/FireDetection&Notification.js
@@ -1,8 +1,38 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView, 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  View
+} from 'react-native';
+import React, {useState} from 'react';
+import MQTT from 'sp-react-native-mqtt';
 import Header from '../../component/header';
 
 export default function FireDetectionNotification() {
+
+  const [Type, setType] = useState('');
+
+  MQTT.createClient({
+    uri: 'mqtt://192.168.166.182:1883',
+    clientId: 'your_client_id',
+  })
+    .then(function (client) {
+      client.on('connect', function () {
+        console.log('connected');
+
+      });
+      client.on('message', function (msg) {
+        console.log(msg.data);
+        setType(msg.data);
+      });
+
+      client.connect();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
   return (
     <SafeAreaView>
       <ScrollView>
